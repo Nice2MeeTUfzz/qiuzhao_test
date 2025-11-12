126a7c005204e783ce2ca2de3a87f60e37f5b517
Yuki <781950209@qq.com>
2025-11-12 13:55:49 +0800

    提交Leetcode今日习题

--- src/LeetCode/相交链表160.java
+++ src/LeetCode/相交链表160.java
@@ -46,8 +46,11 @@ class ListNode {
     int val;
     ListNode next;
 
+    ListNode() {}
+
     ListNode(int x) {
         val = x;
         next = null;
     }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
