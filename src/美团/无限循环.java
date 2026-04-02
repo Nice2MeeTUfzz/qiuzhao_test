543028a3520f2a263533b1f0245c20b3653a57fe
Yoel <781950209@qq.com>
2026-04-02 09:53:29 +0800

    feat(美团): add infinite loop solution for array problem

--- src/美团/无限循环.java
+++ src/美团/无限循环.java
@@ -1,4 +1,29 @@
 package 美团;
 
+import java.util.Scanner;
+
+// </https://codefun2000.com/p/P4646>
 public class 无限循环 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int left = 0, right = 1;
+            int count = 0;
+            while (right < n) {
+                if (arr[left] < arr[right]) {
+                    right++;
+                } else {
+                    left = right;
+                    right++;
+                }
+            }
+            System.out.println(right - left);
+        }
+    }
 }
